94bbad79bc66bd2943c30b2da1ec15ce222f70de
ure-shi <os.stewart.05@gmail.com>
2025-04-15 13:57:46 +1000

    added stopwatch

--- FSharpTransformer/Main.fs
+++ FSharpTransformer/Main.fs
@@ -6,6 +6,8 @@ open System.Diagnostics
 let model53M = readModel "model.bin" "tokenizer.bin"
 
 let stopwatch = Stopwatch.StartNew()
+
 tellStory model53M mostLikely
+
 stopwatch.Stop()
 printfn "Elapsed time: %f seconds" (stopwatch.Elapsed.TotalSeconds)
